ce7173bd288c448e4894f88de452f3ed1dadd504
Ronak Shah <ronakshahultimate@gmail.com>
2016-07-31 20:37:05 -0700

    quick commit

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='gitc',    # This is the name of your PyPI-package.
+    version='1.0',                          # Update the version number for new releases
+    scripts=['gitcomplete']                  # The name of your scipt, and also the command you'll be using for calling it
+)
