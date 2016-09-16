cb15faa84516ce97f252c30c309aadbd9288979c
Ronak Shah <ronakshahultimate@gmail.com>
2016-09-15 23:00:13 -0700

    Quick Commit

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main(int argc, char *argv[]) {
   std::string str = "";
 
   if (argc < 2) {
+    //This is your default commit msg. Change this if you want!
     str = "Quick Commit";
   }
+
   else {
     for (int j = 1; j < argc; j++) {
       str += argv[j];
@@ -18,6 +20,12 @@ int main(int argc, char *argv[]) {
   str = "git commit -m '" + str + "'";
   std::system(str.c_str());
   std::system("git status");
+  /*
+    Uncomment if you want to be extra lazy - don't worry, there are no bad
+    consequences to running 'git push' if you don't have any remotes
+  */
+
+  //std::system("git push");
 
   std::cout<< std::endl << std::endl << "Commit Message: " << str << std::endl;
 
