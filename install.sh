4636a495b317b086a1ed09723f1f0e5322429094
Ronak Shah <ronakshah@srnd.org>
2017-11-15 07:37:11 +0000

    fixed

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@ rm -rf ~/.gitc
 mkdir ~/.gitc
 cp gitc.sh ~/.gitc/main.sh
 echo "Adding Aliases"
-echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.bashrc #works for most people
-echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.bash_profile #for mac
-echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.zshrc #for people using oh-my-zsh
+echo "alias gitc='bash ~/.gitc/main.sh'" >> ~/.bashrc #works for most people
+echo "alias gitc='bash ~/.gitc/main.sh'" >> ~/.bash_profile #for mac
+echo "alias gitc='bash ~/.gitc/main.sh'" >> ~/.zshrc #for people using oh-my-zsh
+echo "alias gitc='bash ~/.gitc/main.sh'" >> ~/.profile #for everyone people
 
 echo "Sourcing Aliases"
 source ~/.bashrc
 source ~/.bash_profile
 source ~/.zshrc
+source ~/.profile
