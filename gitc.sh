b099f2aa70d7cb69eb433f05f5df259289bcb40f
Ronak Shah <ronakshahultimate@gmail.com>
2018-04-20 22:22:03 -0700

    Added a real install script, an uninstall script, and tried to add some stuff to the gitc core

--- gitc.sh
+++ gitc.sh
@@ -1,7 +1,25 @@
-git add -A
+#!/bin/bash
+
+set -e
 if [ $# -eq 0 ]; then
+	git add -A
     git commit -m "Pushing Quick Commit";
 else
-    git commit -m "$*";
+	if [ '${*,,}' = "bug fix" ]; then
+	  echo "Please, you can do better than this"
+	  exit 1
+	else
+		if [ '${*,,}' = "validation bug fix" ]; then 
+			echo "I could do this all day, do better"
+			exit 1
+		else
+			git add -A
+			git commit -m "$*";
+		fi
+	fi
 fi
 git push;
+# if [[ $* = *"bug fix"* ]]; then
+# 	  echo "It's there!"
+# 	fi
+    
--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
-chmod +x gitc.sh
-rm -rf ~/.gitc
-mkdir ~/.gitc
-cp gitc.sh ~/.gitc/main.sh
-echo "Adding Aliases"
-echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.bashrc #works for most people
-echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.bash_profile #for mac
-echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.zshrc #for people using oh-my-zsh
+sudo chmod +x gitc.sh
+sudo cp gitc.sh /usr/local/bin/gitc 
 
-echo "Sourcing Aliases"
-source ~/.bashrc
-source ~/.bash_profile
-source ~/.zshrc
+# rm -rf ~/.gitc
+# mkdir ~/.gitc
+# cp gitc.sh ~/.gitc/main.sh
+# echo "Adding Aliases"
+# echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.bashrc #works for most people
+# echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.bash_profile #for mac
+# echo "alias gitc='sh ~/.gitc/main.sh'" >> ~/.zshrc #for people using oh-my-zsh
+
+# echo "Sourcing Aliases"
+# source ~/.bashrc
+# source ~/.bash_profile
+# source ~/.zshrc
